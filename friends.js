a4b2a69e6e1e15f7c98b521721e87563ffbc9d61
Alex Alexander <a.alexander62442@gmail.com>
2022-11-10 17:36:56 -0700

    Add a for statement

---
+++ friends.js
@@ -0,0 +1,15 @@
+var num = [13, 45, 32, 86, 55, 74, 2, 99]
+
+for (i = 0; i <= 100; i++) {
+    if (num[i] >= 30)
+    console.log(num[i])
+}
+
+
+
+
+//for for statements: i = 0 is the initialization. 
+//The second part - i <= 100, is called the condition.
+//The third part is the final expression (i++)
+
+//the code block beneath the for statement: 
